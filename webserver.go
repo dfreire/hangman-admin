f7f24336be9d0ad54700e9041e8f0452af3e0f4a
dfreire <dario.freire@gmail.com>
2014-10-04 16:24:22 +0100

    update

---
+++ webserver.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+)
+
+func main() {
+	m := martini.Classic()
+	m.Use(render.Renderer())
+
+	m.Get("/", func(r render.Render) {
+        r.Redirect("signin")
+	})
+
+	m.Get("/signin", func(r render.Render) {
+		r.HTML(200, "signin", "", render.HTMLOptions{Layout: "_layout"})
+	})
+
+	m.Get("/dashboard", func(r render.Render) {
+		r.HTML(200, "dashboard", "", render.HTMLOptions{Layout: "_layout"})
+	})
+
+	m.Run()
+}
