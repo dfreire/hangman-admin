21b0c5bbd5ba0014a42ca960cd8477221db15faf
dfreire <dario.freire@gmail.com>
2014-10-07 22:33:43 +0100

    update

--- webserver.go
+++ webserver.go
@@ -14,23 +14,11 @@ func main() {
 	})
 
 	m.Get("/sign-in", func(r render.Render) {
-		r.HTML(200, "sign-in", "", render.HTMLOptions{Layout: "_signed-out"})
+		r.HTML(200, "_signed-out", "")
 	})
 
-	m.Get("/sign-out", func(r render.Render) {
-        r.Redirect("sign-in")
-	})
-
-	m.Get("/reset-password", func(r render.Render) {
-		r.HTML(200, "reset-password", "", render.HTMLOptions{Layout: "_signed-out"})
-	})
-
-	m.Get("/dashboard", func(r render.Render) {
-		r.HTML(200, "dashboard", "", render.HTMLOptions{Layout: "_signed-in"})
-	})
-
-	m.Get("/games", func(r render.Render) {
-		r.HTML(200, "games", "", render.HTMLOptions{Layout: "_signed-in"})
+	m.Get("/admin", func(r render.Render) {
+		r.HTML(200, "_signed-in", "")
 	})
 
 	m.Run()
