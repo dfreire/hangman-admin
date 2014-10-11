e3fdf2acd01e9edb60777dae88e67cf0b387fedb
dfreire <dario.freire@gmail.com>
2014-10-11 13:07:01 +0100

    update

--- webserver.go
+++ webserver.go
@@ -3,8 +3,8 @@ package main
 import (
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
-    "net/http"
-    "strings"
+	"net/http"
+	"strings"
 )
 
 func main() {
@@ -12,40 +12,52 @@ func main() {
 	m.Use(render.Renderer())
 
 	m.Get("/", func(req *http.Request, r render.Render) {
-        r.Redirect("/sign-in")
-    })
+		r.Redirect("/sign-in")
+	})
 
 	m.Get("/sign-out", func(req *http.Request, r render.Render) {
-        r.Redirect("/sign-in")
-    })
+		r.Redirect("/")
+	})
 
 	m.Get("/sign-in", func(req *http.Request, r render.Render) {
-        if isSpider(req) {
-            r.Status(200)
-        } else {
-            r.HTML(200, "_signed-out", "")
-        }
+		if isSpider(req) {
+			renderSpider(r)
+		} else {
+			renderPublic(r)
+		}
 	})
 
 	m.Get("/reset-password", func(req *http.Request, r render.Render) {
-        if isSpider(req) {
-            r.Status(200)
-        } else {
-            r.HTML(200, "_signed-out", "")
-        }
+		if isSpider(req) {
+			renderSpider(r)
+		} else {
+			renderPublic(r)
+		}
 	})
 
 	m.Get("/admin/**", func(req *http.Request, r render.Render) {
-        if isSpider(req) {
-            r.Status(200)
-        } else {
-            r.HTML(200, "_signed-in", "")
-        }
+		if isSpider(req) {
+			r.Redirect("/")
+		} else {
+			renderAdmin(r)
+		}
 	})
 
 	m.Run()
 }
 
 func isSpider(req *http.Request) bool {
-    return strings.Contains(req.URL.RawQuery, "_escaped_fragment_")
+	return strings.Contains(req.URL.RawQuery, "_escaped_fragment_")
+}
+
+func renderSpider(r render.Render) {
+	r.HTML(200, "spider-template", "")
+}
+
+func renderPublic(r render.Render) {
+	r.HTML(200, "public-template", "")
+}
+
+func renderAdmin(r render.Render) {
+	r.HTML(200, "admin-template", "")
 }
