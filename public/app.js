aa240eb0b29550554bcc097faf37f40b4fcd6a46
dfreire <dario.freire@gmail.com>
2014-10-15 21:12:24 +0100

    update

--- public/app.js
+++ public/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('HangmanAdmin', []);
+var app = angular.module('HangmanAdmin', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise("/sign-in");
+
+	$stateProvider
+		.state('sign-in', {
+	  		url: "/sign-in",
+	  		templateUrl: "partials/sign-in.html",
+	  		controller: 'SignInController'
+		});
+});
+
+app.controller('SignInController', function($scope) {
+
+});
