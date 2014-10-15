420521046ff9a9a03ec89747f2bdba32e87e7c52
dfreire <dario.freire@gmail.com>
2014-10-15 22:01:45 +0100

    update

--- public/app.js
+++ public/app.js
@@ -1,13 +1,34 @@
 var app = angular.module('HangmanAdmin', ['ui.router']);
 
+app.config(function($locationProvider) {
+	$locationProvider.hashPrefix('!');
+});
+
 app.config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise("/sign-in");
+	$urlRouterProvider.otherwise('/sign-in');
 
 	$stateProvider
 		.state('sign-in', {
-	  		url: "/sign-in",
-	  		templateUrl: "partials/sign-in.html",
-	  		controller: 'SignInController'
+	  		url: '/sign-in',
+	  		views: {
+	  			"menu": {
+	  				templateUrl: '/elements/public-menu.html'
+	  			},
+	  			"content": {
+	  				templateUrl: '/elements/sign-in.html',
+	  				controller: 'SignInController'
+	  			}
+	  		}
+		}).state('reset-password', {
+	  		url: '/reset-password',
+	  		views: {
+	  			"menu": {
+	  				templateUrl: '/elements/public-menu.html'
+	  			},
+	  			"content": {
+	  				templateUrl: '/elements/reset-password-panel.html'
+	  			}
+	  		}
 		});
 });
 
